89d4508648ddac5fbe95035722c713b1658d998c
xiewu <xiewu@cmcm.com>
2019-06-04 13:49:08 +0800

    优化提交，随机阅读时间和时段奖励判断

--- brush_the_article/httautogetcoin.js
+++ brush_the_article/httautogetcoin.js
@@ -1,20 +1,25 @@
 auto.waitFor();
 device.keepScreenDim();
-var maxScrollTime = 18;
-var pageScrollTime = maxScrollTime;  // 页面最大可滚动次数
+var baseScrollTime = 10;
+var pageScrollTime = 18;  // 页面最大可滚动次数
 var titles = [];
 var nowIndex = 0;
 var maxTitle = 0;
 
 // 执行跳转
 function navigator() {
+  if(currentActivity() != "com.cashtoutiao.account.ui.main.MainTabActivity") {
+    return;
+  }
     var title = titles[nowIndex];
     nowIndex ++;
     if (title) {
         //click(title.bounds().left,title.bounds().top-200);
         try{
+          //collectTimeAward();
           title.parent().click();
         } catch(err) {
+          console.log("点击去文章失败", err);
           navigator();
           return;
         }
@@ -40,13 +45,17 @@ function initSelect() {
     //titles = id("tv_stick").find();
     titles = id("iv_shielding").find();
     maxTitle = titles.length;
-    pageScrollTime = maxScrollTime;
+    pageScrollTime = randomScrollCount();
 }
 
 // 进入页面滚动操作
 function scrollDownAction() {
+    if(currentActivity() != "com.cashtoutiao.news.ui.NewsDetailActivity" && 
+    currentActivity() != "com.cashtoutiao.alivideodetail.AliVideoDetailActivity") {
+      return;
+    }
     if (pageScrollTime < 0) {
-        pageScrollTime = maxScrollTime;
+        pageScrollTime = randomScrollCount();
         back();
         sleep(1000);
         navigator()
@@ -56,8 +65,44 @@ function scrollDownAction() {
     swipe(device.width/2,500,device.width/2,300,500);
     sleep(1000);
     pageScrollTime --;
+    clickMore();
     console.log("滚动次数",pageScrollTime);
     scrollDownAction();
+}
+
+// 随机一个最大滚动次数
+function randomScrollCount() {
+  return Math.ceil(baseScrollTime + Math.random()*10)
+}
+
+// 点击查看更多
+function clickMore() {
+  try{
+    var more = textStartsWith("展开全文").findOnce();
+    if (more) {
+      more.parent().click();
+      sleep(600);
+      console.log("点击了一次展开更多");
+    }
+  } catch(err) {
+    console.log("展开更多失败", err);
+  }
+}
+
+// 收集时段奖励
+function collectTimeAward() {
+  try{
+    var awardBtn = id("receive_layout").text("点击领取").findOnce();
+    if (awardBtn) {
+      awardBtn.parent().click();
+      sleep(600);
+      text("忽略").findOnce().click();
+      sleep(400);
+      console.log("领取了一次时段奖励");
+    }
+  }catch(err) {
+    console.log("领取时段奖励失败", err);
+  }
 }
 
 initSelect();
